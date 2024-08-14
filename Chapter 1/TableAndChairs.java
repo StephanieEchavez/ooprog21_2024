205e452890c3ac3434a1494469fecaaee4d39db9
Stephanie Echavez <162314552+tsokolet@users.noreply.github.com>
2024-08-14 20:34:47 +0800

    Update TableAndChairs.java

--- Chapter 1/TableAndChairs.java
+++ Chapter 1/TableAndChairs.java
@@ -1,11 +1,11 @@
 public class TableAndChairs{
   public static void main(String[] args) {
-    System.out.println("*                        *");
-    System.out.println("*                        *");
-    System.out.println("*                        *");
-    System.out.println("******  **********  ******");
-    System.out.println("*    *  *        *  *    *");
-    System.out.println("*    *  *        *  *    *");
-    System.out.println("*    *  *        *  *    *");
+    System.out.println("x                        x");
+    System.out.println("x                        x");
+    System.out.println("x                        x");
+    System.out.println("xxxxxx  xxxxxxxxxx  xxxxxx");
+    System.out.println("x    x  x        x  x    x");
+    System.out.println("x    x  x        x  x    x");
+    System.out.println("x    x  x        x  x    x");
   }
 }
