06bce75b32092feea51cf2caf8cd3aa6b563c2bd
Stephanie Echavez <162314552+StephanieEchavez@users.noreply.github.com>
2024-09-27 08:35:02 +0700

    Update Billing.java

--- Chapter 4/Billing.java
+++ Chapter 4/Billing.java
@@ -1,1 +1,24 @@
+public class Billing {
+   
+      public static double compBill(double price) {
+         double total = price + (price * 0.08);
+         return total;
+      }
+      
+      public static double compBill(double price, int quantity) {
+         double total = (price * quantity) * ((price * quantity) * 0.08);
+         return total;
+      }
 
+      public static double compBill(double price, int quantity, int coupon) {
+         double disPrice = (price * quantity) - coupon;
+         double total = disPrice * (disPrice + 0.08);
+         return total;
+      }
+      
+   public static void main(String[] args) {
+      System.out.println("The total price for the bill is: " + compBill(23.2));
+      System.out.println("The total price for the bill is: " + compBill(23.2, 2));
+      System.out.println("The total price for the bill is: " + compBill(23.2, 3, 10));
+   } 
+}
