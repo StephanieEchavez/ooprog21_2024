e2d0389d49a4235c6ee55640e579d8f1104346b9
Stephanie Echavez <162314552+StephanieEchavez@users.noreply.github.com>
2024-10-04 08:42:56 +0700

    Create FirstAddressExcercise.java

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,13 @@
+
+class FirstAddressExcercise {
+    public static void main(String[] args) {
+        System.out.println("First Java application");
+        displayAddress();
+    }
+    public static void displayAddress(){
+        System.out.println("Google LLC" + 
+                            "\n1600 Amphitheatre Parkway" +
+                            "\nMountain View, CA 94043" + 
+                            "\nUnited States");
+    }
+}
